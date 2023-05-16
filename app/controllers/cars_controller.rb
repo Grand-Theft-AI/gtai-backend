423b0fba0b8eb00959d87b969815d52aca5e609d
SunGodApollo <apple.ortiz@hotmail.com>
2023-05-16 15:17:44 -0700

    finished create func

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,4 +4,19 @@ class CarsController < ApplicationController
         cars = Car.all
         render json: cars
     end
+
+    def create 
+        car = Car.create(car_params)
+        if car.valid?
+            render json: car
+        else
+            render json: car.errors, status: 422
+    end
+
+
+
+    private
+    def car_params
+        params.require(:car).permit(:make, :model, :year, :mileage, :image, :price, :description, :street, :city, :state, :zip, :user_id)
+    end
 end
