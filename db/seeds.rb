9eac61b1308a156a2ea8684b63f879f7854a37cb
gnostertag <ostertag2000@gmail.com>
2023-05-16 10:03:46 -0700

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1=User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+user2=User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+
+cars1=[
+  {
+    make: "Tesla",
+    model: "Model S",
+    year: 2020,
+    mileage: 5000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvZkwSkgsvgEOTdPHW3Lw-rVJPTM_rp0R9Lg&usqp=CAU",
+    price: 89000.00,
+    description: "Everything is Electric",
+    street: "1337 Los Santos Way",
+    city: "San Andreas",
+    state: "CA",
+    zip: "90210"
+  }
+]
+cars2=[
+  {
+    make: "Lucid",
+    model: "Air",
+    year: 2023,
+    mileage: 200,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp3-9mrkSEr99-IulJY2vjwjdwAsMqJilpSA&usqp=CAU",
+    price: 8700.00,
+    description: "Everything is Also Electric",
+    street: "808 Mafia",
+    city: "Vice City",
+    state: "FL",
+    zip: "33101"
+  }
+]
+
+cars1.each do |car|
+  user1.cars.create(car)
+  puts "created: #{car}"
+end
+cars2.each do |car|
+  user2.cars.create(car)
+  puts "created: #{car}"
+end
