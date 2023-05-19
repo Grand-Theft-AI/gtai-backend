028de5428dbcfc120db791e734f26c6b1efb99f5
gnostertag <ostertag2000@gmail.com>
2023-05-19 11:43:49 -0700

    missing comma

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :cars
-  devise_for :users
+  devise_for :users,
  path: '',
  path_names: {
   sign_in: 'login',
   sign_out: 'logout',
   registration: 'register',
- }
+ },
  controllers: {
   sessions: 'users/sessions',
   registrations: 'user/registrations',
