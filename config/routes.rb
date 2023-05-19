ff5bb375ac60460889ab8ec035045d02bfc632f1
gnostertag <ostertag2000@gmail.com>
2023-05-19 11:39:34 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   resources :cars
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ path: '',
+ path_names: {
+  sign_in: 'login',
+  sign_out: 'logout',
+  registration: 'register',
+ }
+ controllers: {
+  sessions: 'users/sessions',
+  registrations: 'user/registrations',
+ }
 end
