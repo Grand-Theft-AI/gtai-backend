c5f2678774a51d95c034fa2e95c561ad7cdcbd6a
gnostertag <ostertag2000@gmail.com>
2023-05-19 13:15:23 -0700

    backend connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
  has_many :cars
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+ devise :database_authenticatable, :registerable,
+ :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
